d8c9aa52589c546b340f3bacc932dff25b95f518
Lucas Wendel <lucas.wmartins@hotmail.com>
2023-04-17 23:28:25 -0300

    Add method Get e GetById PersonController

--- LW.ApiDotNet6.Application/Services/Interfaces/IPersonService.cs
+++ LW.ApiDotNet6.Application/Services/Interfaces/IPersonService.cs
@@ -6,4 +6,6 @@ namespace LW.ApiDotNet6.Application.Services.Interfaces;
 public interface IPersonService
 {
     Task<ResultService<PersonDTO>> CreateAsync(PersonDTO personDTO);
+    Task<ResultService<ICollection<PersonDTO>>> GetAsync();
+    Task<ResultService<PersonDTO>> GetByIdAsync(int id);
 }
